b35e3af4f1db4c2ca0e8336100bfd3281b58950b
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-05-28 19:55:03 -0400

    Se añade sitemaps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: "https://lightboard.cl",
+	integrations: [
+		sitemap(),
+		sitemap({
+			xslURL: "/sitemap.xsl",
+		}),
+	],
+});
